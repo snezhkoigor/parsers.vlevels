0825b1a4e1e597d66fc00a1f7361e571c17a2261
i.s.sergeevich@yandex.ru <snezhkoigor>
2016-08-15 18:00:44 +0300

    add pdf copy from cme ftp

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Storage;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        // Commands\Inspire::class,
+        Commands\GetFilesFromFTP::class
     ];
 
     /**
@@ -24,7 +25,18 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('getFilesFromFTP')
+            ->before(function () {
+                if (count(Storage::disk('public')->files(env('CME_PARSER_SAVE_FOLDER') . '/' . date("Y") . '/' . env('CME_BULLETIN_FOLDER_PREFIX') . '/')) != 0) {
+                    echo 'Директория не пустая.';
+                    die;
+                }
+            })
+            ->everyThirtyMinutes()
+            ->withoutOverlapping()
+            ->after(function () {
+                echo 'Завершено';
+            });
+
     }
 }

2a4a8aa2d2ddec0e2b7d93625ed964b299792fed
i.s.sergeevich@yandex.ru <snezhkoigor>
2016-08-29 17:07:42 +0300

    new parser for all pairs

--- public/index.php
+++ public/index.php
@@ -49,12 +49,12 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 phpinfo();
 
 die;
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+//$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+//
+//$response = $kernel->handle(
+//    $request = Illuminate\Http\Request::capture()
+//);
+//
+//$response->send();
+//
+//$kernel->terminate($request, $response);

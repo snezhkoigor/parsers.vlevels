95beb85984658e4c0c810f5f3541cdbaaac0deaa
i.s.sergeevich@yandex.ru <snezhkoigor>
2016-09-15 18:17:31 +0300

    add many monthes to parsers

--- app/Services/Cme/Aud.php
+++ app/Services/Cme/Aud.php
@@ -29,7 +29,7 @@ class Aud extends Base
         $this->pair = self::PAIR_AUD;
 
         parent::__construct($option_date, $pdf_files_date);
-        var_dump($this->option_date);die;
+
         $this->pair_with_major = self::PAIR_AUD.self::PAIR_USD;
         $this->option = DB::table($this->table)
             ->where(
@@ -40,7 +40,7 @@ class Aud extends Base
             )
             ->orderBy('_expiration')
             ->first();
-
+        var_dump($this->option);die;
         $this->table_day = 'cme_day_'.strtolower($this->pair_with_major);
         $this->table_total = 'cme_bill_'.strtolower($this->pair_with_major).'_total';
         $this->table_month = 'cme_bill_'.strtolower($this->pair_with_major).'_'.strtolower($this->option->_option_month);
